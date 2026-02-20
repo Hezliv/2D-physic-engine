0edb4c17c46210fd097c0d6b1490421dea6b0005
Elina Varenyk <elinavarenyk23@gmail.com>
2026-02-20 21:56:02 +0200

    Upgrade collision resolution method for rigidbodies

--- 2DPhysicEngine.cpp
+++ 2DPhysicEngine.cpp
@@ -1,8 +1,10 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window/Mouse.hpp>
 #include <iostream>
-#include <cmath>
+#include <math.h>
 #include <vector>
+
+# define M_PI 3.141592653589793238462643383279502884L
 using namespace std;
 using namespace sf;
 
@@ -43,22 +45,39 @@ public:
 		height = radius * 2;
 		width = radius * 2;
 		this->position = position;
-		mass = 2.0f;
+		mass = (0.5 * height * height * M_PI) / 1000;
 	}
 };
 
 class Ball { // decide what do with class wrapper
 public:
+	Vector2f position;
 	RigidBody* physics;
 	CircleShape* visual;
+	Vector2f normal;
+
+	Vector2f GetCenter()
+	{
+
+	}
+
+	void SetPosition(Vector2f& pos)
+	{
+		position = pos;
+	}
+
 };
+
+// rewrite method KeepInBounds because of problem with stucking circle objects
+// optimize the Distance method
+// organize code, transfer methods which useful for circle objects to thir own class
 
 class PhysicEngine
 {
 	vector<PhysBody*> objects;
-	Vector2f gravity = { 0, 400.0f }; // для монитора лучше испльзовать 980 весто реальных 9.8
+	Vector2f gravity = { 0, 500.0f }; //for monitor would be better use 980 raither our 9.8 for gravity, here I use 400 for more smooth but not like in moon(300.0f)
 	Vector2u size;
-	float resistance = 0.3f;
+	float resistance = 0.2f;
 	float air_resistance = 0.1f;
 public:
 	void KeepInBound()
@@ -125,30 +144,50 @@ public:
 
 	void ResolveCollision()
 	{
-		
 		for(int i = 0; i < objects.size(); i++)
 		{
 			for(int j = i + 1; j < objects.size(); j++)
 			{			
-				if (Distance(objects[i]->position, objects[j]->position) < (objects[i]->width / 2 + objects[j]->width / 2)) // only for circles
+				if (Distance(Vector2f( objects[i]->GetCenter().x, objects[i]->GetCenter().y), Vector2f(objects[j]->GetCenter().x, objects[j]->GetCenter().y)) < (objects[i]->width / 2 + objects[j]->width / 2)) // only for circles
 				{
-					//cout << objects[j]->mass << "\n";
-					//cout << objects[i]->position.y << " " << objects[j]->position.y << "\n";
-					float mass = objects[i]->mass + objects[j]->mass; // вес
+					float mass = 1 / objects[i]->mass + 1 / objects[j]->mass; // вес
 
-					Vector2f pos_diff = objects[i]->position - objects[j]->position; // разница между расстояниями
+					Vector2f pos_diff = objects[i]->GetCenter() - objects[j]->GetCenter(); // разница между расстояниями
 					Vector2f velocity_diff = objects[i]->velocity - objects[j]->velocity; // разница скорости
 
-					float prod = velocity_diff.x * (objects[i]->position.x - objects[j]->position.x) + velocity_diff.y * (objects[i]->position.y - objects[j]->position.y);
-					float dist_sq = pos_diff.x * pos_diff.x + pos_diff.y * pos_diff.y;
-					if (dist_sq < 0.000001f) continue;
-					float collision = (1.8f * prod) / (mass * dist_sq);
+					float dist = Distance(objects[i]->GetCenter(), objects[j]->GetCenter());
+					float e = min(objects[i]->resilience, objects[j]->resilience);
+					if (dist < 0.001f) dist = 0.001f;
+					Vector2f normal = pos_diff / dist;
+					
+					float scalar = velocity_diff.x * normal.x + velocity_diff.y * normal.y;
+					float impulse = -(1 + e) * scalar / mass;
+					
+					Vector2f jm = normal * impulse;
+					
+					//cout << objects[i]->velocity.x << " " << objects[i]->velocity.y << "\n";
+					//cout << objects[j]->velocity.x << " " << objects[j]->velocity.y << "\n";
 
-					objects[i]->velocity -= collision * objects[j]->mass * pos_diff; // обмен скоростями
-					objects[j]->velocity += collision * objects[i]->mass * pos_diff;
+					objects[i]->velocity += jm / objects[i]->mass; // обмен скоростями
+					objects[j]->velocity -= jm / objects[j]->mass;
+
+					float penetration = (objects[i]->height / 2 + objects[j]->height / 2) - Distance(objects[i]->GetCenter(), objects[j]->GetCenter());
+
+					float percent = 0.8f;
+					float slop = 0.01f;
+
+					Vector2f correction = normal * (max(penetration - slop, 0.0f) / (1 / objects[i]->mass + 1 / objects[j]->mass) * percent);
+
+					objects[i]->position += correction / objects[i]->mass; // обмен скоростями
+					objects[j]->position -= correction / objects[j]->mass;
 				}
 			}
 		}
+	}
+
+	void PositionCorrection(PhysBody* obj1, PhysBody* obj2)// redo 
+	{
+		
 	}
 
 	void ApplyForce(Vector2f force)
@@ -166,9 +205,17 @@ public:
 		return sqrtf((pow((first.x - second.x), 2) + pow((first.y - second.y), 2)));
 	}
 
-	void MouseVoid(Vector2f mouse_pos, float radius, float strength)
+	vector<PhysBody*> GetObjects()
 	{
-		for(auto i : objects)
+		return objects;
+	}
+};
+
+class MouseInteraction {
+public:
+	void MouseVoid(vector<PhysBody*>& objects, Vector2f mouse_pos, float radius, float strength)
+	{
+		for (auto i : objects)
 		{
 			Vector2f center = i->GetCenter();
 			Vector2f diff = center - mouse_pos;
@@ -196,16 +243,16 @@ int main()
 	circle1.setFillColor(Color::White);
 	circle1.setPosition({ 100, 10 });
 	CircleShape circle2(20);
-	circle2.setFillColor(Color::Red);
+	circle2.setFillColor(Color::White);
 	circle2.setPosition({ 100, 100 });
 	CircleShape circle3(20);
-	circle3.setFillColor(Color::Blue);
+	circle3.setFillColor(Color::White);
 	circle3.setPosition({ 300, 100 });
 	CircleShape circle4(30);
-	circle4.setFillColor(Color::Green);
+	circle4.setFillColor(Color::White);
 	circle4.setPosition({ 700, 100 });
 	CircleShape circle5(10);
-	circle5.setFillColor(Color::Yellow);
+	circle5.setFillColor(Color::White);
 	circle5.setPosition({ 500, 100 });
 	Clock clock;
 	RigidBody circle_body1(circle1.getRadius(), circle1.getPosition());
@@ -213,12 +260,8 @@ int main()
 	RigidBody circle_body3(circle3.getRadius(), circle3.getPosition());
 	RigidBody circle_body4(circle4.getRadius(), circle4.getPosition());
 	RigidBody circle_body5(circle5.getRadius(), circle5.getPosition());
-	circle_body1.mass = 1.0f;
-	circle_body2.mass = 2.0f;
-	circle_body3.mass = 3.0f;
-	circle_body4.mass = 4.0f;
-	circle_body5.mass = 5.0f;
 
+	MouseInteraction m;
 	eng.AddObj(&circle_body1);
 	eng.AddObj(&circle_body2);
 	eng.AddObj(&circle_body3);
@@ -239,8 +282,8 @@ int main()
 		if (Mouse::isButtonPressed(Mouse::Button::Left))
 		{
 			Vector2i mouse = Mouse::getPosition(*window);
-
-			eng.MouseVoid(Vector2f(mouse.x, mouse.y), 40.0f, 1000000.0f);
+			auto temp = eng.GetObjects();
+			m.MouseVoid(temp, Vector2f(mouse.x, mouse.y), 100.0f, 1000000.0f);
 		}
 
 		circle1.setPosition(circle_body1.position);
